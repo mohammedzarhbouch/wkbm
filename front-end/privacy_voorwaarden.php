85e8a0b93011133d92f2c12789f1e1d96b217755
Kevinsimon046 <113350066+Kevinsimon046@users.noreply.github.com>
2024-02-20 14:54:36 +0100

    added privacy en voorwaarden page en opgemaakt, deze is af!

---
+++ front-end/privacy_voorwaarden.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/privacy_voorwaarden.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="hamburger-menu">
+    <input id="menu__toggle" type="checkbox" />
+    <label class="menu__btn" for="menu__toggle">
+      <span></span>
+    </label>
+
+    <ul class="menu__box">
+      <li><a class="menu__item" href="#">Profiel<br>_______________________</a></li>
+      <li><a class="menu__item" href="#">Instellingen<br>_______________________</a></li>
+      <li><a class="menu__item" href="#">Rekening<br>_______________________</a></li>
+      <li><a class="menu__item" href="#">Privacy en voorwaarden<br>_______________________</a></li>
+      <li><a class="menu__item" href="#">Uitloggen<br>_______________________</a></li>
+    </ul>
+  </div>
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Privacy en Voorwaarden</title>
+  <style>
+
+  </style>
+</head>
+<body>
+
+  <header>
+    <h1>Privacy en Voorwaarden</h1>
+  </header>
+
+  <section>
+    <h2>Privacybeleid</h2>
+    <p>
+    Wij respecteren jouw privacy. Verzamelde gegevens worden veilig bewaard, 
+    niet gedeeld zonder toestemming, en dienen alleen voor de verbetering van onze diensten.
+    <br><br> Algemene Voorwaarden: Door onze site te gebruiken, ga je akkoord met de voorwaarden. 
+    Houd je accountgegevens veilig, gebruik de site verantwoord, en respecteer ons auteursrecht.
+    Wijzigingen kunnen plaatsvinden, dus blijf op de hoogte.
+    </p>
+  </section>
+
+
+  <footer>
+    ©2024 WKBM Finance. Alle rechten voorbehouden.
+  </footer>
+
+</body>
+</html>
+
+</body>
+</html>
