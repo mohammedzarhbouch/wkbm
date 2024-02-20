f3ade4d28aa0ae1d75095b87e33e264452dc9e27
Wadih2006 <512373@vistacollege.nl>
2024-02-20 18:34:20 +0100

    verbeteringen

--- front-end/home.php
+++ front-end/home.php
@@ -17,34 +17,79 @@ if (!isset($_SESSION['loggedin'])) {
 </head>
 <body>
     <section>
+    <div class="hamburger-menu">
+
+    <input id="menu__toggle" type="checkbox" />
+        <label class="menu__btn" for="menu__toggle">
+            <span></span>
+        </label>
+
+        <?php require_once("../mainmenu.php"); ?>
+    </div>
+    
         <div class="header">WKBM FINANCE</div>
 
-        <table class="content-table">
-            <thead>
-                <tr>
-                    <th>Datum</th>
-                    <th>Locatie</th>
-                    <th>Uitgaven</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1 feb</td>
-                    <td>Jumbo</td>
-                    <td>-9,05</td>
-                </tr>
-                <tr class="active-row">
-                    <td>5 feb</td>
-                    <td>AliBaba</td>
-                    <td>+230,99</td>
-                </tr>
-                <tr>
-                    <td>14 feb</td>
-                    <td>MyTicket</td>
-                    <td>-555,67</td>
-                </tr>
-            </tbody>
-        </table>
+        <div style="max-height: 400px; overflow-y: auto; overflow-x: hidden;">
+            <table class="content-table">
+                <thead>
+                    <tr>
+                        <th>Datum</th>
+                        <th>Locatie</th>
+                        <th>Uitgaven</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1 feb</td>
+                        <td>Jumbo</td>
+                        <td>-9,05</td>
+                    </tr>
+                    <tr class="active-row">
+                        <td>5 feb</td>
+                        <td>AliBaba</td>
+                        <td>+230,99</td>
+                    </tr>
+                    <tr>
+                        <td>14 feb</td>
+                        <td>MyTicket</td>
+                        <td>-555,67</td>
+                    </tr>
+                    <tr class="active-row">
+                        <td>22 feb</td>
+                        <td>Gymshark</td>
+                        <td>-49,95</td>
+                    </tr>
+                    <tr>
+                        <td>22 feb</td>
+                        <td>Gymshark</td>
+                        <td>-49,95</td>
+                    </tr>
+                    <tr class="active-row">
+                        <td>22 feb</td>
+                        <td>Gymshark</td>
+                        <td>-49,95</td>
+                    </tr>
+                    <tr>
+                        <td>22 feb</td>
+                        <td>Gymshark</td>
+                        <td>-49,95</td>
+                    </tr>
+                    <tr class="active-row">
+                        <td>22 feb</td>
+                        <td>Gymshark</td>
+                        <td>-49,95</td>
+                    </tr>
+                    <tr>
+                        <td>22 feb</td>
+                        <td>Gymshark</td>
+                        <td>-49,95</td>
+                    </tr>
+                    <!-- Additional rows can be added here -->
+                </tbody>
+            </table>
+        </div>
+
+        <button type="button">Overboeken</button>
     </section>
 
 </body>
---
+++ mainmenu.php
@@ -0,0 +1,7 @@
+<ul class="menu__box">
+    <li><a class="menu__item" href="#">Profiel<br>_______________________</a></li>
+    <li><a class="menu__item" href="#">Instellingen<br>_______________________</a></li>
+    <li><a class="menu__item" href="home.php">Rekening<br>_______________________</a></li>
+    <li><a class="menu__item" href="privacy_voorwaarden.php">Privacy en voorwaarden<br>_______________________</a></li>
+    <li><a class="menu__item" href="index.html">Uitloggen<br>_______________________</a></li>
+</ul>
