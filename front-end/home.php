c9eea1f849d37b59822a8a1e3a00817f4a7fb3ee
Wadih2006 <512373@vistacollege.nl>
2024-02-20 10:12:31 +0100

    table toegevoegd en nieuwe achtergrond

--- front-end/home.php
+++ front-end/home.php
@@ -12,9 +12,40 @@ if (!isset($_SESSION['loggedin'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/home.css">
+    <title>Rekeningoverzicht</title>
 </head>
 <body>
-    <a href="../back-end/logout.php">Logout</a>
+    <section>
+        <div class="header">WKBM FINANCE</div>
+
+        <table class="content-table">
+            <thead>
+                <tr>
+                    <th>Datum</th>
+                    <th>Locatie</th>
+                    <th>Uitgaven</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1 feb</td>
+                    <td>Jumbo</td>
+                    <td>-9,05</td>
+                </tr>
+                <tr class="active-row">
+                    <td>5 feb</td>
+                    <td>AliBaba</td>
+                    <td>+230,99</td>
+                </tr>
+                <tr>
+                    <td>14 feb</td>
+                    <td>MyTicket</td>
+                    <td>-555,67</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+
 </body>
 </html>
