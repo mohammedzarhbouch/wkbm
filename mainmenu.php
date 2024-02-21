1a8e4d53682c2925f3cda7c21202106df3879fe3
Kevinsimon046 <113350066+Kevinsimon046@users.noreply.github.com>
2024-02-21 13:38:20 +0100

    added profiel page

--- mainmenu.php
+++ mainmenu.php
@@ -1,5 +1,5 @@
 <ul class="menu__box">
-    <li><a class="menu__item" href="#">Profiel<br>_______________________</a></li>
+    <li><a class="menu__item" href="profiel.php">Profiel<br>_______________________</a></li>
     <li><a class="menu__item" href="#">Instellingen<br>_______________________</a></li>
     <li><a class="menu__item" href="home.php">Rekening<br>_______________________</a></li>
     <li><a class="menu__item" href="privacy_voorwaarden.php">Privacy en voorwaarden<br>_______________________</a></li>
