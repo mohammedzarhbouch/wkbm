7b4b37079ce62521f0c1fee008e4473248a4abd3
512187 <512187@vistacollege.nl>
2024-02-21 11:37:27 +0100

    Settingsphp + Settingscss

---
+++ front-end/settings.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- <link rel="Website icon" type="jpg" href="../foto/IMG_2600.jpg"> -->
+    <link rel="stylesheet" href="../css/settings.css">
+    <title>Settings</title>
+</head>
+<body>
+    <section>
+        <div class="hamburger-menu">
+
+            <input id="menu__toggle" type="checkbox" />
+                <label class="menu__btn" for="menu__toggle">
+                    <span></span>
+                </label>
+        
+                <?php require_once("../mainmenu.php"); ?>
+            </div>
+        <div class="header">WKBM FINANCE</div>
+        <div id="text3"><h3>Instellingen<h3></div>
+            <div id="formBox">
+            <div class="text2">Donkere Modus</div>
+                <button id="button1" type="button">Aan/Uit</button>
+                <button id="button2" type="button">Wachtwoord veranderen</button>
+                <button id="button3" type="button">Opslaan</button>
+                </div>
+            <div id="text">
+        </div>
+    </section>
+</body>
+</html>
